694276a335d8edeaf8e3a728054437ba460075f8
michellebu <michellebu@berkeley.edu>
2011-12-25 19:27:53 -0600

    scrolling

--- site/public/js/index.js
+++ site/public/js/index.js
@@ -1,11 +1,21 @@
-$(document).ready(
-  function() {
-	  $('#showlogin').click(
-	    function() {
-	      $('#showlogin').attr('disabled', 'disabled');
-	      $('#loginform').slideToggle(300);
-	      $('#showlogin').fadeToggle(300);
-	    }
-	  );
-  }
-);
+$(document).ready(function(){
+	$('#showlogin').click(function(){
+	  $('#showlogin').attr('disabled', 'disabled');
+	  $('#loginform').slideToggle(300);
+	  $('#showlogin').fadeToggle(300);
+	});
+	
+	$(".scroll").click(function(event){
+    event.preventDefault();
+ 
+    var full_url = this.href;
+ 
+    var parts = full_url.split("#");
+    var trgt = parts[1];
+ 
+    var target_offset = $("#"+trgt).offset();
+    var target_top = target_offset.top;
+ 
+    $('html, body').animate({scrollTop:target_top}, 500);
+  });
+});
