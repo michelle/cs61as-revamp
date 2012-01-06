ba75d0c4d8e008bd27142990807532674d9d180a
michellebu <michellebu@berkeley.edu>
2012-01-06 04:13:41 -0600

    whif statement should take care of all bad urls

--- server.js
+++ server.js
@@ -958,12 +958,6 @@ app.get('/homework/:lessonId', loadUser, checkPermit('canReadLesson'), loadProgr
 // TODO: add view solution
 app.get('/solutions/:type/:lessonId', loadUser, checkPermit('canReadLesson'), loadProgress, function(req, res) {
   trace('GET /solutions/:type/:lessonId');
-  if (!(req.params.type in ['homework', 'extra'])) {
-    req.flash('error', "Whoops! The url you just went to does not exist.");
-    res.redirect('/default');
-    return;
-  }
-
   if (req.currentLesson && req.currentLesson[req.params.type]) {
     req.currentUser.currentLesson = req.currentLesson.number;
     req.currentUser.save(function(err) {
